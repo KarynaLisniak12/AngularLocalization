2b7f096450a1dd3903e5d99693ecfa976d51f8ec
karyna.lisniak <karyna.lisniak@gmail.com>
2018-12-13 11:56:33 +0200

    Add localization for all modules

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PagesRoutingModule } from './pages-routing.module';
 import { PagesComponent } from './pages/pages.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   imports: [
     CommonModule,
-    PagesRoutingModule
+    PagesRoutingModule,
+    TranslateModule
   ],
   declarations: [
     PagesComponent
